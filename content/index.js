ac6a72624450e5c3a59442b27da4efde07013337
darakuneko <tvkids@gmail.com>
2023-01-18 23:08:17 +0900

    update: refactor

--- content/index.js
+++ content/index.js
@@ -1,16 +1,18 @@
 $(function(){
+    const sendKeycode  = (keycode) => document.getElementById("keyboard")
+        .dispatchEvent(new KeyboardEvent("keydown", { keyCode: keycode  }))
+
     $('#tab-content').find('input').on("keydown", e => {
         if(e.keyCode === 13){
             e.preventDefault();
             const id = $(e.target).attr('id')
             $("#keyboard").trigger("focus")
             if (e.shiftKey) {
-              document.getElementById("keyboard").dispatchEvent(new KeyboardEvent("keydown", { keyCode: 74  }))          
+                sendKeycode(74)
             } else {
-                document.getElementById("keyboard").dispatchEvent(new KeyboardEvent("keydown", { keyCode: 75  }))
+                sendKeycode(75)
             }
             $(`#${id}`).trigger("focus")
-            console.log(id)
         }
     })
 })
